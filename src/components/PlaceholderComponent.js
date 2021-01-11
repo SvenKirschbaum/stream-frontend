a14eb66938bfe591d8bca34ac62859d5f97581a3
Sven Kirschbaum <sven@kirschbaum.me>
2021-01-11 03:19:32 +0100

    Updated Placeholder

--- src/components/PlaceholderComponent.js
+++ src/components/PlaceholderComponent.js
@@ -6,16 +6,8 @@ function PlaceholderComponent() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Placeholder
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     );
