437a3becc84d68fc2030ca63df27c240fca017b7
Sven Kirschbaum <sven@kirschbaum.me>
2023-04-30 00:24:27 +0200

    :construction_worker: Migrate from cra to vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,3 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import {defineConfig, splitVendorChunkPlugin} from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// const apiHost = 'https://stream.elite12.de';
+const apiHost = 'http://localhost:8080';
+
+const secure = apiHost.includes('https');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), svgrPlugin(), basicSsl(), splitVendorChunkPlugin()],
+    server: {
+        port: 3000,
+        // Required to work with oauth provider
+        https: true,
+        proxy: {
+            '/api': {
+                target: apiHost,
+                changeOrigin: secure,
+                secure,
+            },
+            '/api/sock': {
+                target: apiHost,
+                ws: true,
+                changeOrigin: secure,
+                secure,
+            }
+        },
+    },
+    preview: {
+        port: 3000
+    },
+    build: {
+        outDir: 'build'
+    },
+    resolve: {
+        alias: {
+            // See https://github.com/sockjs/sockjs-client/issues/439
+            'sockjs-client': 'node_modules/sockjs-client/dist/sockjs.js'
+        }
+    }
+});
